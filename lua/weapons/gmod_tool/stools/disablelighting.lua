eeff46d2d1bf7249d78b88daeaaaa95d526001da
Iced Coffee <me@icedd.coffee>
2020-05-14 18:30:54 +1000

    dupe support + functionality change

--- lua/weapons/gmod_tool/stools/disablelighting.lua
+++ lua/weapons/gmod_tool/stools/disablelighting.lua
@@ -3,36 +3,78 @@ TOOL.Name			= "Disable Lighting"
 TOOL.Command		= nil
 TOOL.ConfigName		= ""
 
-if ( CLIENT ) then
-	language.Add( "Tool.disablelighting.name", "Disable Lighting" )
-	language.Add( "Tool.disablelighting.desc", "Disables lighting on props." )
-	language.Add( "Tool.disablelighting.left", "Disable Lighting" )
-	language.Add( "Tool.disablelighting.right", "Enable Lighting" )
-	language.Add( "disablelighting_name", "Name:" )
-	TOOL.Information = { "left", "right" }
+if CLIENT then
+	language.Add("Tool.disablelighting.name", "Disable Lighting")
+	language.Add("Tool.disablelighting.desc", "Disables lighting on props.")
+	language.Add("Tool.disablelighting.left", "Disable Lighting")
+	language.Add("Tool.disablelighting.right", "Enable Lighting")
+	language.Add("disablelighting_name", "Name:")
+	TOOL.Information = {"left", "right"}
+else
+	util.AddNetworkString("EntityLightingChange")
 end
 
-local function IsLightingDisabled(ent)
-	for k,v in pairs(ent:GetChildren()) do
-		if v:GetClass() == "prop_physics_nolighting" then
-			return true
+local function DisableEntityLighting(ent, toggle)
+	if not IsValid(ent) then return end
+	
+	if toggle then
+		ent.RenderOverride = function(ent)
+			render.SuppressEngineLighting(true)
+			ent:DrawModel()
+			render.SuppressEngineLighting(false)
 		end
+		
+		ent.LightingDisabled = true
+	else
+		ent.RenderOverride = nil
+		ent.LightingDisabled = false
 	end
-	return false
 end
+
+hook.Add("OnEntityCreated", "lightingcheck", function(ent)
+	local toggle = ent:GetNWBool("disablelighting", false)
+	
+	if CLIENT and toggle then
+		DisableEntityLighting(ent, true)
+	end
+end)
+
+net.Receive("EntityLightingChange", function()
+	local toggle = net.ReadBool()
+	local ent = net.ReadEntity()
+	if not IsValid(ent) then return end
+	
+	if toggle then
+		DisableEntityLighting(ent, true)
+	else
+		DisableEntityLighting(ent, false)
+	end
+end)
+
+local function DupeLighting(ply, ent, data)
+	timer.Simple(0.05, function()
+		net.Start("EntityLightingChange")
+			net.WriteBool(data[1])
+			net.WriteEntity(ent)
+		net.Broadcast()
+	end)
+	
+	ent:SetNWBool("disablelighting", true)
+end
+duplicator.RegisterEntityModifier("DisableEntityLighting", DupeLighting)
 
 function TOOL:LeftClick(trace)
 	local entity = trace.Entity
 	if entity:IsWorld() or entity:IsPlayer() then return false end
+	
 	if SERVER then
-		if IsLightingDisabled(entity) then return true end
-		local ent = ents.Create("prop_physics_nolighting")
-		ent:SetPos(entity:GetPos())
-		ent:SetAngles(entity:GetAngles())
-		ent:SetModel(entity:GetModel())
-		ent:SetParent(entity)
-		ent:SetRenderMode(RENDERMODE_TRANSALPHA)
-		ent:Spawn()
+		net.Start("EntityLightingChange")
+			net.WriteBool(true)
+			net.WriteEntity(entity)
+		net.Broadcast()
+		
+		entity:SetNWBool("disablelighting", true)
+		duplicator.StoreEntityModifier(entity, "DisableEntityLighting", {true})
 	end
 	return true
 end
@@ -41,15 +83,15 @@ end
 function TOOL:RightClick(trace)
 	local entity = trace.Entity
 	if entity:IsWorld() or entity:IsPlayer() then return false end
-	if CLIENT then
-		entity:SetNoDraw(false)
-	else
-		entity:SetNoDraw(false)
-		for k,v in pairs(entity:GetChildren()) do
-			if v:GetClass() == "prop_physics_nolighting" then
-				v:Remove()
-			end
-		end
+	
+	if SERVER then
+		net.Start("EntityLightingChange")
+			net.WriteBool(false)
+			net.WriteEntity(entity)
+		net.Broadcast()
+		
+		entity:SetNWBool("disablelighting", false)
+		duplicator.StoreEntityModifier(entity, "DisableEntityLighting", {false})
 	end
 	return true
 end
